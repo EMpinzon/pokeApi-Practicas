12c67c35a519cf58ff12af0f63819111445c9bb1
EMpinzon <EdgarYMauricio@outlook.es>
2023-09-28 22:32:34 -0500

    feat: :sparkles: nueva funcionalidad, mostrar estadisticas pokemon

--- scrpt.js
+++ scrpt.js
@@ -63,11 +63,42 @@ function createPokemon(pokemon){
 
     const cardBack = document.createElement('div');
     cardBack.classList.add('pokemon-block-back');
-    cardBack.textContent="info";
+    cardBack.appendChild(progressBars(pokemon.stats))
 
     cardContainer.appendChild(card);
     cardContainer.appendChild(cardBack);
     pokemonContainer.appendChild(flipcard);
+}
+
+
+function progressBars (stats){
+    const statsContainer = document.createElement('div');
+    statsContainer.classList.add("stats-container");
+    for (let i = 0;i<3 ; i++){
+
+        const stat = stats[i];
+        const statPercent = stat.base_stat / 3 + "%";
+        const statContainer = document.createElement('div');
+        statContainer.classList.add("stat-container");
+        const statName =  document.createElement('p');
+        statName.textContent = stat.stat.name;
+        const progress = document.createElement("div");
+        progress.classList.add("progress");
+        const progressBar = document.createElement('div');
+        progressBar.classList.add("progress-bar");
+        
+        
+        progressBar.style.width = statPercent;
+        progressBar.textContent = stat.base_stat;
+
+        progress.appendChild(progressBar);
+        statContainer.appendChild(statName);
+        statContainer.appendChild(progress);
+        statsContainer.appendChild(statContainer);
+
+    }
+    return statsContainer;
+
 }
 
 function removechildNodes(parent){ 
